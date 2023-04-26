0c8585f1b3721e9d0bfd50fa5f19de0538e2d6fb
inmar salinas <inmarsalinas@yahoo.com>
2023-04-26 13:39:38 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(function(drivers){
+        return drivers.toLowerCase() === string.toLowerCase()
+    })
+}
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(drivers){
+        return drivers.toLowerCase().substring(0, string.length) === string.toLowerCase()
+    })
+}
+function matchName(drivers,string){
+    return drivers.filter(function(drivers){
+        return drivers.name === string
+    })
+}
